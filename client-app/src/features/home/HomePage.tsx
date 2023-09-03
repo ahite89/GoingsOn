3e039c571642d6a2061f7201b8c816ebc25bc73d
Alexander Hite <alexander.hite@aspirion.com>
2023-09-03 13:47:59 -0400

    ui overhaul

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Container, Header, Image, Segment, Button } from "semantic-ui-react";
 
 export default function HomePage() {
     return (
-        <Container>
-            <h1>Home page</h1>
-            <h3>Go to <Link to='/activities'>Activities</Link></h3>
-        </Container>
+       <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as="h1" inverted>
+                    <Image size="massive" src="/assets/Cat1.jpeg" alt="logo" style={{marginBottom: 12}} />
+                    Goings On
+                </Header>
+                <Header as="h2" inverted content="Welcome to Goings On" />
+                <Button as={Link} to="/activities" size="huge" inverted>
+                    Take me to the goings on
+                </Button>
+            </Container>
+       </Segment>
     )
 }
